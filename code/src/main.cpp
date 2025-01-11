b4ab6b76790009437d288b0b1cae24b7dd179416
Greg Cormier <gcormier@gmail.com>
2025-01-11 11:45:22 -0500

    moar

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -4,21 +4,12 @@
 #include <Arduino.h>
 #include "tpms_silencer.h"
 
-/*
-Each bit/symbol: 0/1 is 100us long. sent at a rate of 10kHz
-144 symbols sent in each TPMS packet: taking 14.4ms (+ 200us warmup)
-All Packets are sent 40ms apart, then we deep-sleep.
-If PERIODIC_TX is defined then wakeup after 7s * wakeuplimit (which is potentially variable)
-BACKOFF will double the retransmit period each time until MAX_LIMIT.
-*/
-
 #define PIN_EN PA0           // PA0, Pin 13, Arduino 10
 #define PIN_FSK PA1          // PA1, Pin 12, Arduino 9
 #define PIN_ASK PA2          // PA2, Pin 11, Arduino 8
 #define PIN_BUTTON PA7       // PA7, Pin 6, PCINT7
 #define INTERRUPT_PIN PCINT7 // interrupt for PA7
 
-// #define PERIODIC_TX // periodic retransmissions. disable for button-press tx only
 // #define BACKOFF    // double transmit period everytime? disable to have a fixed period retrans.
 
 #define ENHIGH (bitSet(PORTA, PIN_EN))
@@ -44,16 +35,23 @@ BACKOFF will double the retransmit period each time until MAX_LIMIT.
 #endif
 
 #define SHORTPACKETUS 420  // Short packet duration in uS
-#define LONGPACKETUS  856   // Long packet duration in uS
+#define LONGPACKETUS 856   // Long packet duration in uS
 #define BREAKPACKETUS 2000 // Break between packets in uS
-
+#define TWEAK 20           // Typically this will be your period (eg 50kHz interrupt, 20uS)
+                           // but you can fine tune as needed
 #define NO_PACKETS 1
 #define PACKET_LEN 2
-//#define PACKETSIZE (PACKET_LEN * 8)
+// #define PACKETSIZE (PACKET_LEN * 8)
 #define PACKETSIZE 13 // We only actually want 13 bits here
+
+// on is
+// 0b 10000100 0b11111000
+// off is
+// 0b 10000100 0b01110000
+
 const unsigned char packets[NO_PACKETS][PACKET_LEN] = {
     // packet 0
-    {B10000110, B11110000}};
+    {B10000100, B11111000}};
 
 volatile unsigned char packet = 0; // which packet is being sent 0 - NO_PACKETS-1
 volatile unsigned int currentBit;  // which bit in that packet is next
@@ -64,7 +62,7 @@ volatile unsigned int wakeupCounter = 0;         // how many 8s watchdog timeout
 volatile unsigned int wakeuplimit = LIMIT_START; // How many 8-second wakeups before transmit again
 
 volatile unsigned int tickCounter = 0; // count the number of interrupts, so we can determine how long to pulse for
-volatile unsigned int sendLow = 0;    // Tell us to send the low signal
+volatile unsigned int sendLow = 0;     // Tell us to send the low signal
 // setup functions
 
 // 50khz Interrupt for an 4MHz clock
@@ -83,7 +81,7 @@ void setupInterrupt4()
   TCCR1B |= (1 << WGM12);
 
   // Prescaler 1
-   TCCR1B |= (1 << CS00);
+  TCCR1B |= (1 << CS00);
 
   // Output Compare Match A Interrupt Enable
   TIMSK1 |= (1 << OCIE1A);
@@ -153,7 +151,7 @@ void setup()
   // down to 1.8V (datasheet page 238)
   cli();
   CLKPR = (1 << CLKPS0); // Set prescaler to divide by 2 (8 MHz / 2 = 4 MHz)
-  sei(); // Re-enable interrupts
+  sei();                 // Re-enable interrupts
   for (byte i = 0; i < 13; i++)
     pinMode(i, INPUT);
 
@@ -171,14 +169,6 @@ void setup()
   pinMode(PIN_ASK, OUTPUT);
   pinMode(PIN_BUTTON, INPUT_PULLUP);
 
-#ifdef PERIODIC_TX
-  /* Clear the reset flags. */
-  MCUSR = 0;
-
-  /* set watchdog interrupt with prescalers */
-  WDTCSR = 1 << WDP0 | 1 << WDP3 | 1 << WDIE | 1 << WDE; /* 8.0 seconds */
-#endif
-
 #if F_CPU == 16000000L
   setupInterrupt16();
 #elif F_CPU == 8000000L
@@ -191,7 +181,6 @@ void setup()
   disableTX();
 
   wakeupCounter = wakeuplimit; // force an immediate transmit
-
 }
 
 // runtime functions
@@ -201,7 +190,7 @@ ISR(TIMER1_COMPA_vect)
 {
   if (transmitting)
   {
-    if (tickCounter > 0)  // If we need to keep this signal high for longer, keep going
+    if (tickCounter > 0) // If we need to keep this signal high for longer, keep going
     {
       tickCounter--;
       return;
@@ -210,7 +199,7 @@ ISR(TIMER1_COMPA_vect)
     {
       sendLow = 0;
       ASKLOW;
-      tickCounter = SHORTPACKETUS / 24;
+      tickCounter = SHORTPACKETUS / TWEAK;
       return;
     }
 
@@ -225,31 +214,21 @@ ISR(TIMER1_COMPA_vect)
     {
       // We need to send a 1 which means be high for LONGPACKETUS, then be low for SHORTPACKETUS
       ASKHIGH;
-      tickCounter = LONGPACKETUS / 24;  // We will lose some precision here, but it's close enough
-      //tickCounter = 30;
+      tickCounter = LONGPACKETUS / TWEAK; // We will lose some precision here, but it's close enough
+      // tickCounter = 30;
       sendLow = 1;
     }
     else
     {
       ASKHIGH;
-      tickCounter = SHORTPACKETUS / 24;  // We will lose some precision here, but it's close enough
-      //tickCounter = 4;
+      tickCounter = SHORTPACKETUS / TWEAK; // We will lose some precision here, but it's close enough
+      // tickCounter = 4;
       sendLow = 1;
     }
 
     currentBit++;
   }
 }
-
-#ifdef PERIODIC_TX
-// watchdog timer is setup by setupInterrupt8(). kicks roughly 7-8s at 3v.
-ISR(WDT_vect)
-{
-  // reset WDIE to ensure next watchdog uses the interrupt too (dont reboot!)
-  WDTCSR |= bit(WDIE);
-  wakeupCounter++;
-}
-#endif
 
 // button interrupt is setup by setupInterrupt8()
 ISR(PCINT0_vect)
@@ -316,21 +295,12 @@ void loop()
     for (int i = 0; i < NO_PACKETS; i++)
     {
       sendPacket(i);
-      delay(1000);
+      delay(100);
     }
-    delay(1000);
+    delay(100);
     // reset to wait for next tx
     wakeupCounter = 0;
-
-#ifdef BACKOFF
-    // double the wakeuplimit each time to back-off and save battery
-    wakeuplimit *= 2;
-#ifdef MAX_LIMIT
-    // but only up to this limit:
-    wakeuplimit = min(wakeuplimit, MAX_LIMIT);
-#endif
-#endif
   }
 
-  //sleepyTime();
+  sleepyTime();
 }
